174d971016c412e562fd3d3e8c90ffa088c2b256
n2omatt <n2o.matt@gmail.com>
2016-04-29 14:35:36 -0300

    - Add the doxygen comments.

--- termcolor/termcolor.h
+++ termcolor/termcolor.h
@@ -99,23 +99,50 @@ constexpr int UNDERLINE = 4;
 ////////////////////////////////////////////////////////////////////////////////
 // Functions                                                                  //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief Constructs the escape string sequence.
+///@param code - The escape code integer.\n
+///              It won't check for code validity. Is users responsabillity
+///              to ensure that code is valid. So the safer use is with one
+///              termcolor constants.
+///@returns A escape sequence in format of "\033[Xm",
+///         where X is the code given as argument.
 std::string codeToString(int code);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Config                                                                     //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief Global Color Modes - MODIFY ALL COLORING FUNCTIONS. \n
+///This class defines how the coloring functions will behave. \n
+///To change the behavior assign a new value to Config.colorMode.
 class Config
 {
- public:
+public:
     enum class ColorMode
     {
-        OnlyIfTerminal = 0,
-        Always         = 1,
-        Never          = 2,
-        Default        = OnlyIfTerminal
+        OnlyIfTerminal = 0, ///< Color escapes codes are only inserted if the
+                            ///  stdout is assigned to a tty.
+                            ///  \n
+                            ///  This enables code to don't care the type of
+                            ///  the stdout i.e. if a pipe, file, or tty.
+                            ///  \n
+                            ///  If the stdout isn't assigned to a tty all
+                            ///  coloring functions won't modify the string.
+                            ///  For the streams functions, them won't put
+                            ///  any escape sequence in the stream.
+
+        Always = 1, ///< Don't care for where the stdout is assigned.
+                    ///  Put the coloring escape sequences anyway.
+
+        Never = 2, ///< Don't care for where the stdout is assigned.
+                   ///  DO NOT put the coloring escape sequences anyway.
+
+        Default = OnlyIfTerminal ///< Same as OnlyIfTerminal.
     };
 
+    ///@brief The value of colorMode will control, how the escape sequences
+    ///are used in the program. To override the current behavior assign a
+    ///new value to this variable.
     static ColorMode colorMode;
  };
 
@@ -124,39 +151,123 @@ class Config
 // Stream Functions                                                           //
 ////////////////////////////////////////////////////////////////////////////////
 // Foreground Colors //
-std::ostream& grey   (std::ostream &os);
-std::ostream& red    (std::ostream &os);
-std::ostream& green  (std::ostream &os);
-std::ostream& yellow (std::ostream &os);
-std::ostream& blue   (std::ostream &os);
+
+///@brief Put the grey foreground escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& grey(std::ostream &os);
+
+///@brief Put the red foreground escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& red(std::ostream &os);
+
+///@brief Put the green foreground escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& green(std::ostream &os);
+
+///@brief Put the yellow foreground escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& yellow(std::ostream &os);
+
+///@brief Put the blue foreground escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& blue(std::ostream &os);
+
+///@brief Put the magenta foreground escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
 std::ostream& magenta(std::ostream &os);
-std::ostream& cyan   (std::ostream &os);
-std::ostream& white  (std::ostream &os);
+
+///@brief Put the cyan foreground escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& cyan(std::ostream &os);
+
+///@brief Put the white foreground escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& white(std::ostream &os);
+
 
 // Background Colors //
-std::ostream& on_grey   (std::ostream &os);
-std::ostream& on_red    (std::ostream &os);
-std::ostream& on_green  (std::ostream &os);
-std::ostream& on_yellow (std::ostream &os);
-std::ostream& on_blue   (std::ostream &os);
+
+///@brief Put the grey background escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& on_grey(std::ostream &os);
+
+///@brief Put the red background escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& on_red(std::ostream &os);
+
+///@brief Put the green background escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& on_green(std::ostream &os);
+
+///@brief Put the yellow background escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& on_yellow(std::ostream &os);
+
+///@brief Put the blue background escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& on_blue(std::ostream &os);
+
+///@brief Put the magenta background escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
 std::ostream& on_magenta(std::ostream &os);
-std::ostream& on_cyan   (std::ostream &os);
-std::ostream& on_white  (std::ostream &os);
+
+///@brief Put the cyan background escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& on_cyan(std::ostream &os);
+
+///@brief Put the white background escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& on_white(std::ostream &os);
+
 
 // Attributes //
-std::ostream& blink    (std::ostream &os);
-std::ostream& bold     (std::ostream &os);
+
+///@brief Put the blink attribute escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& blink(std::ostream &os);
+
+///@brief Put the bold attribute escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& bold(std::ostream &os);
+
+///@brief Put the concealed attribute escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
 std::ostream& concealed(std::ostream &os);
-std::ostream& dark     (std::ostream &os);
-std::ostream& reverse  (std::ostream &os);
+
+///@brief Put the dark attribute escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& dark(std::ostream &os);
+
+///@brief Put the reverse attribute escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
+std::ostream& reverse(std::ostream &os);
+
+///@brief Put the underline attribute escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
 std::ostream& underline(std::ostream &os);
 
+
+// Reset //
+
+///@brief Put the reset escape sequence in the stream.
+///@note This function is affected by the value of Config.colorMode
 std::ostream& reset(std::ostream &os);
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Colored Function                                                           //
 ////////////////////////////////////////////////////////////////////////////////
+///@brief Builds the colored output in one function. \n
+///       It will put the foreground color, then the background and after all
+///       the attributes if there are any.
+///@param str - The string that will be colored.
+///@param foreground - A valid foreground color code. (Mandatory)
+///@param background - A valid background color code. (Optional)
+///@param attributes - A valid attributes codes. (Optional)
+///@note This function is affected by the value of Config.colorMode.
+///@warning This function will not check the validity of the color codes,
+///so is user's responsibility to ensure that them are valid. \n
+///The best bet is use the termcolor constants.
 std::string colored(const std::string &str,
                     int foreground,
                     int background = termcolor::NONE,
@@ -166,12 +277,13 @@ std::string colored(const std::string &str,
 ////////////////////////////////////////////////////////////////////////////////
 // Functor Class                                                              //
 ////////////////////////////////////////////////////////////////////////////////
+///
 class options
 {
 public:
     options(int foreground,
-            int background = termcolor::NONE,
-            const std::vector<int> &attributes = std::vector<int>()) :
+          int background = termcolor::NONE,
+          const std::vector<int> &attributes = std::vector<int>()) :
         m_foreground(foreground),
         m_background(background),
         m_attributes(attributes)
@@ -190,6 +302,8 @@ private:
     std::vector<int> m_attributes;
 };
 
-NS_TERMCOLOR_END
 
+
+
+NS_TERMCOLOR_END
 #endif // defined(__AmazingCow_TermColor_Cpp_termcolor_h__) //
